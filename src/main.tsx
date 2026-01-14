5ce6ce564e21990f58e74b843b5b0c38343bc82b
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-01-14 09:51:42 -0500

    deleted unncessary imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
